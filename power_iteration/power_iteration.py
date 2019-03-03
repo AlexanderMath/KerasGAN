f49a2e34f7ed2ad5068af6307393ba694675892a
Alexander <alexander.mathiasen@gmail.com>
2019-03-03 16:09:01 +0100

    Added an implementation of power iteration for w-gan so we can bypass computing svd.

---
+++ power_iteration/power_iteration.py
@@ -0,0 +1,64 @@
+"""
+	Thanks to taki0112 for sharing an implementation of spectral normalization under the MIT license. 
+
+		https://github.com/taki0112/Spectral_Normalization-Tensorflow
+
+	The code below is a slight modification the NumPy code from Wikipedia 
+
+		https://en.wikipedia.org/wiki/Power_iteration
+		https://en.wikipedia.org/wiki/Rayleigh_quotient (compute eigenvalue given eigenvect)
+
+	The NumPy code was rewritten to Tensorflow inspired by taki0112. 
+
+	The power_iteration methods below computes the largest spectral value, that is, the
+	square root of the largest eigenvalue of A.T @ A, see: 
+
+		https://en.wikipedia.org/wiki/Singular_value
+
+"""
+import numpy as np
+import tensorflow as tf
+import time
+
+
+# --- NUMPY CODE ---
+def l2_normalize(b): return b / np.linalg.norm(b)
+
+def power_iteration_np(A, num_simulations=10, init_vect=None, approximate_scale=1.00):
+	A = A.T @ A # TODO: IS THERE ANY DIFFERENCE TO A @ A.T? IF NOT, USE THE SMALLEST!
+	if init_vect is None: 	b_k = np.random.rand(A.shape[1])
+	else:					b_k = init_vect
+
+	for k in range(num_simulations): 	b_k 	= l2_normalize(A @ b_k )
+ 
+	return np.sqrt(b_k.T @ A @ b_k / (b_k.T @ b_k  )) * approximate_scale  # add 10 percent so we always shoot a bit larger 
+# --- END NUMPY CODE ---
+
+
+# --- TF CODE --- 
+def power_iteration_tf(A, sess, num_simulations=10, init_vect=None, approximate_scale=1.00):
+	A = tf.transpose(A) @ A
+	if init_vect is None: 	b_k = tf.get_variable("b_k"+str(time.time()), (A.shape[0], 1), initializer=tf.random_normal_initializer(), trainable=False)
+	else:					b_k = init_vect
+
+	sess.run(b_k.initializer)
+
+	for k in range(num_simulations):	 b_k = tf.nn.l2_normalize( A @ b_k )
+ 
+	return np.sqrt(sess.run(tf.transpose(b_k) @ A @ b_k / (tf.transpose(b_k) @ b_k))) * approximate_scale  # add 10 percent so we always shoot a bit larger 
+# --- END TF CODE --- 
+
+# --- RENAME FASTEST (see test_speed.py for experiment) --- 
+def power_iteration(A, num_simulations=10, init_vect=None, approximate_scale=1.00): 
+	return power_iteration_np(A, num_simulations, init_vect, approximate_scale)
+
+if __name__ == "__main__": 
+
+	a = np.random.normal(0, 1, size=(100, 100)).astype(np.float32)
+
+	sess = tf.Session()
+	print("SVD: \t%.8f"% 	np.max(sess.run(tf.linalg.svd(a)[0])))
+	print("NumPy: \t%.8f"% 	power_iteration_np(a, 50))
+	print("TF: \t%.8f"% 	power_iteration_tf(a, sess, 50)[0][0]) 
+
+
---
+++ power_iteration/test_speed.py
@@ -0,0 +1,68 @@
+"""
+	Compares the speed of different approaches to computing largest eigenvalue. 
+	Quite surprisingly the NumPy variant performed the fastest! 
+
+	The plot in 'test_speed.pdf' display some weird behaviour for the tensorflow 
+	implementation, maybe moving stuff back and forth from GPU takes time?
+	For now the NumPy variant is fast enough for the w-gan. 
+
+	TODO: Figure out if there is anything wrong with the GPU implementation. 
+	Also, when training the w-gan everything all the matrices lives in the GPU
+	RAM, so figuring out what is wrong on the GPU aught to make matters faster. 
+
+"""
+
+import numpy as np
+import tensorflow as tf
+from power_iteration import *
+import matplotlib.pyplot as plt
+import time
+
+fig, ax = plt.subplots(1, 2)
+sess = tf.Session()
+
+time_svd = []
+time_np = []
+time_tf = []
+
+estimate_svd = []
+estimate_np = []
+estimate_tf = []
+
+iterations = 30
+
+for i in range(1, iterations, 1):
+	print("--- [%i / %i] ---"%(i, iterations))
+
+	size = i*100
+
+	a = np.random.normal(0, 1, size=(size, size)).astype(np.float32)
+
+	t0 = time.time()
+	estimate_svd.append(np.max(sess.run(tf.linalg.svd(a)[0])))
+	t1 = time.time()
+	estimate_np.append(power_iteration_np(a, 50))
+	t2 = time.time()
+	estimate_tf.append(power_iteration_tf(a, sess, 50)[0][0])
+	t3 = time.time()
+
+	time_svd.append(t1-t0)
+	time_np.append(t2-t1)
+	time_tf.append(t3-t2)
+
+	for j in range(2): ax[j].cla()
+	ax[0].set_title("Time")
+	ax[0].plot(time_svd, label="TF: SVD") # do np svd?
+	ax[0].plot(time_np, label="NP: Power Iteration")
+	ax[0].plot(time_tf, label="TF: Power Iteration")
+
+	ax[1].set_title("Estimate")
+	ax[1].plot(estimate_svd, label="SVD")
+	ax[1].plot(estimate_np, label="NP: Power Iteration")
+	ax[1].plot(estimate_tf, label="TF: Power Iteration")
+	for j in range(2): ax[j].legend()
+
+	plt.tight_layout()
+	plt.savefig("test_speed.pdf")
+	plt.pause(.1)
+
